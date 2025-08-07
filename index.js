0d24d06fe4a4f30bd31f88d61e7a8ad874f2a08b
Dev-Devanshu <writeds.dev@gmail.com>
2025-08-07 05:43:59 +0530

    date-7/aug

---
+++ index.js
@@ -0,0 +1,70 @@
+
+// // // const obj ={
+// // //     name : "devanshu",
+// // //     class: "23"
+// // // }
+// // // delete obj.class
+// // // console.log(obj)
+// // // // it  is called as one special operator 
+
+
+// // // // nested object 
+// // // // object inside the object 
+
+
+
+// // // }
+// // // }
+
+// // // console.log(person.address.personal)
+
+// // // special opertors are there 
+
+// // // typeof
+// // // delete 
+// // // await
+// // // in
+// // // of
+// // // new
+
+// // // arthmatic -- + - * % 
+
+
+// // // javascript ===== react js 40 % ---  it is all about the concept 
+
+// // // react js  -- react native -- mmobile application --- 40 % 60%
+
+// // // how to create one simple object and comple object ?
+
+// // // adding the function inside the object it will always be unknow
+
+// // const obj={
+// //     name:"devanshu",
+// //     class:"12",
+// //     fn:function(){  //anonymous function 
+// // const value=320
+// // console.log(value)
+// //     }
+// // }
+
+// // arrays --- 
+// // array is called the datastructure and it is used for to store the multiple values 
+// // in the one variable
+
+
+// const data=[12,"12",true,{name:"devanshu"}]
+// console.log(data)
+
+// we have elements in the array and the value that are stored in the elements 
+// those positioing is called as index 
+// index will always start from 0 to n --- 
+// we have 4 types of operations alsoo in thie 
+//  - read 
+// update
+// delete
+// insert 
+
+// method in the array we will see this 
+
+// done with object 
+// in progress with arrays we will try to c over this tomrrow 
