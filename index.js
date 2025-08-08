a314019d7b667ef5a312465557f03b3a644fa3e0
Dev-Devanshu <writeds.dev@gmail.com>
2025-08-08 05:57:11 +0530

    08-aug

--- index.js
+++ index.js
@@ -1,70 +1,205 @@
 
-// // // const obj ={
-// // //     name : "devanshu",
-// // //     class: "23"
+// // // // const obj ={
+// // // //     name : "devanshu",
+// // // //     class: "23"
+// // // // }
+// // // // delete obj.class
+// // // // console.log(obj)
+// // // // // it  is called as one special operator 
+
+
+// // // // // nested object 
+// // // // // object inside the object 
+
+
+
+// // // // }
+// // // // }
+
+// // // // console.log(person.address.personal)
+
+// // // // special opertors are there 
+
+// // // // typeof
+// // // // delete 
+// // // // await
+// // // // in
+// // // // of
+// // // // new
+
+// // // // arthmatic -- + - * % 
+
+
+// // // // javascript ===== react js 40 % ---  it is all about the concept 
+
+// // // // react js  -- react native -- mmobile application --- 40 % 60%
+
+// // // // how to create one simple object and comple object ?
+
+// // // // adding the function inside the object it will always be unknow
+
+// // // const obj={
+// // //     name:"devanshu",
+// // //     class:"12",
+// // //     fn:function(){  //anonymous function 
+// // // const value=320
+// // // console.log(value)
+// // //     }
 // // // }
-// // // delete obj.class
-// // // console.log(obj)
-// // // // it  is called as one special operator 
+
+// // // arrays --- 
+// // // array is called the datastructure and it is used for to store the multiple values 
+// // // in the one variable
 
 
-// // // // nested object 
-// // // // object inside the object 
+// // const data=[12,"12",true,{name:"devanshu"}]
+// // console.log(data)
+
+// // we have elements in the array and the value that are stored in the elements 
+// // those positioing is called as index 
+// // index will always start from 0 to n --- 
+// // we have 4 types of operations alsoo in thie 
+// //  - read 
+// // update
+// // delete
+// // insert 
+
+// // method in the array we will see this 
+
+// // done with object 
+// // in progress with arrays we will try to c over this tomrrow 
 
 
+// // var data =[
+// //     Number,String,Boolean,Object,function
+// // ]
 
-// // // }
-// // // }
+// // var data =[1,"2","4",true,{okay:"its fine"}
+// // ]
+// // console.log(data)
 
-// // // console.log(person.address.personal)
+// // we have lot of ways to create the arrays 
+// // - arrays literals 
+// //  array=[""]  square brackets represents the array 
+// // - arrays constructor
 
-// // // special opertors are there 
+// //  propeties 
+// // ----------------
 
-// // // typeof
-// // // delete 
-// // // await
-// // // in
-// // // of
-// // // new
+// // var data=[1,2,3,4,5,6,7]
+// // what is the lenght for this data 
+// // index will always start from 0 to n 
+// // the data type for these index are called as the string not number 
+// // beacuse these are mapped to the string behind the scen or browser 
+// // console.log(data.length)
+// // console.log(data["1"])  //2
 
-// // // arthmatic -- + - * % 
+
+// //  we have four major method in the array - 
+// // push 
+// // pop
+// // shift 
+// // unshift
 
 
-// // // javascript ===== react js 40 % ---  it is all about the concept 
+// // push -- it will push the element to the array
 
-// // // react js  -- react native -- mmobile application --- 40 % 60%
+// // var  data =[1,2,3,4]
+// // data.pop()
+// // console.log(data)
+// // push element will store the element in the last 
+// // pop will remove the element from the last 
 
-// // // how to create one simple object and comple object ?
+// // shift and unshift 
+// // shift will remove the element from the first 
+// // unshift will add the element to the first 
 
-// // // adding the function inside the object it will always be unknow
+// // var data =[1,3,4,5]
+// // data.unshift("1")
+// // // console.log(data)
 
-// // const obj={
-// //     name:"devanshu",
-// //     class:"12",
-// //     fn:function(){  //anonymous function 
-// // const value=320
+// // arrrays -- 
+// // arrays method 
+// // how to create array -- 
+// // how to add elements to the array 
+// // how to access the element witht the help of index 
+
+// // we have different types of method in the arrays 
+
+// // map--- method 
+
+// // itertate karega sari values ko or with the help of this we can iterate the 
+// // whole element 
+
+
+// // var data=[1,2,3,4]
+// // {data.map((value,index)=>{
+// //     console.log(value)
+// // })}   // it is the way to access the dynamic value 
+
+
+// // var data =[1,2,3,4,5]
+// // // me chahta hu ki  jab me map function use kru to jo bhi array ki values hai vo print ho jaye na ki array ---- 
+// // //  curluy braces ka use hum dynamic value use karne ke liye karte hai
+// // {data.map((value,index)=>{
 // // console.log(value)
-// //     }
-// // }
+// // console.log(index)
+// // })}
 
-// // arrays --- 
-// // array is called the datastructure and it is used for to store the multiple values 
-// // in the one variable
+// // filter method in the arrays 
+// // -----------------------------------
+
+// // it will filter the values depend upon the conditions 
+
+// let users=[
+//     {name:"devnashu",age:12,},
+//     {name:"devnashu2",age:32,},
+//     {name:"devnashu3",age:22,},
+// ]
+
+// // array of object inside this array iam storing three objet 
+// // we will use filter method 
+
+// // it will filter the values according the conditions 
+
+// let add=users.filter((user)=>{
+//     if(user.age>=18){
+//         console.log(user)
+//     }else{
+//         console.log("tum se na ho poayega")
+//     }
+// })
+
+// console.log(add)
 
 
-// const data=[12,"12",true,{name:"devanshu"}]
+// // revserse method in the javascript
+// let data=[1,2,3,4,5,6,7]
+// console.log(data)
+// data.reverse()
 // console.log(data)
 
-// we have elements in the array and the value that are stored in the elements 
-// those positioing is called as index 
-// index will always start from 0 to n --- 
-// we have 4 types of operations alsoo in thie 
-//  - read 
-// update
-// delete
-// insert 
+// sort + revserse 
 
-// method in the array we will see this 
+// let str="hello"
+// let data=str.split("").reverse().join("")
+// console.log(data)   do read about this for tomorrow and with logic and explaination
 
-// done with object 
-// in progress with arrays we will try to c over this tomrrow 
+// splice --- concat --- find ()---- hositing and dom objects 
+
+// class will be 9 to 10 pm---- 
+// patterns + loops concecpt 
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *   pattern in js
+
+// logic building --- 
+
+// -- javascript -- 1 sept
+
+// type script -- 
+//   advance css -- 
+
